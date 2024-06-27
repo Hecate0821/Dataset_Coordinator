5b14550691636ede7b3d0e1e34804d05783f0a22
Hecate <hecate0821@outlook.com>
2024-06-27 15:04:04 +0800

    fix: add log fix bug

--- main.go
+++ main.go
@@ -2,34 +2,49 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 	"net/http"
 	"os"
 	"sync"
+	"time"
 )
 
 type Task struct {
-	Pattern    string `json:"pattern"`
-	Status     int    `json:"status"`
-	WorkerName string `json:"worker_name"`
+	Pattern      string `json:"pattern"`
+	Status       int    `json:"status"`
+	WorkerName   string `json:"worker_name"`
+	AssignedTime string `json:"assigned_time"`
+	FinishedTime string `json:"finished_time"`
+	ExecuteCount int    `json:"execute_count"`
 }
 
 var tasks []Task
 var tasksLock sync.RWMutex
 
+const FINISHED int = 2
+const PROCESSING int = 1
+const UNFINISHED int = 0
+
+const WARNING_EXECUTED_COUNT int = 5
+
 func main() {
+	// 初始化日志
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	logrus.SetOutput(os.Stdout)
+	logrus.SetLevel(logrus.InfoLevel)
+
 	// 初始化任务列表
 	loadTasks()
 
 	r := gin.Default()
 
 	// 获取任务
-	r.POST("/getTask", getTask)
+	r.POST("/getTask", logRequest(getTask))
 	// 更新任务状态
-	r.POST("/updateTask", updateTask)
+	r.POST("/updateTask", logRequest(updateTask))
 
-	r.POST("/withdrawTask", withdrawTask)
+	r.POST("/withdrawTask", logRequest(withdrawTask))
 
 	r.Run(":80")
 }
@@ -37,12 +52,12 @@ func main() {
 func loadTasks() {
 	data, err := os.ReadFile("task.json")
 	if err != nil {
-		fmt.Println("Error reading tasks file:", err)
+		logrus.WithError(err).Error("Error reading tasks file")
 		return
 	}
 	err = json.Unmarshal(data, &tasks)
 	if err != nil {
-		fmt.Println("Error unmarshalling tasks:", err)
+		logrus.WithError(err).Error("Error unmarshalling tasks")
 	}
 }
 
@@ -50,10 +65,29 @@ func getTask(c *gin.Context) {
 	tasksLock.Lock()
 	defer tasksLock.Unlock()
 
+	var request struct {
+		WorkerName   string `json:"worker_name"`
+		ExecuteCount int    `json:"execute_count"`
+	}
+
+	if err := c.BindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	if request.ExecuteCount > WARNING_EXECUTED_COUNT {
+		logrus.WithFields(logrus.Fields{
+			"worker_name":   request.WorkerName,
+			"execute_count": request.ExecuteCount,
+		}).Warn("Exceeded maximum number of execute count")
+	}
+
 	for i, task := range tasks {
-		if task.Status == 0 {
-			task.Status = 1
-			task.WorkerName = c.ClientIP() // 使用请求者的IP作为worker名
+		if task.Status == UNFINISHED {
+			task.Status = PROCESSING
+			task.WorkerName = request.WorkerName
+			task.ExecuteCount = task.ExecuteCount + 1
+			task.AssignedTime = time.Now().In(time.FixedZone("CST", 8*3600)).Format("2006-01-02 15:04:05")
 			tasks[i] = task
 			saveTasks()
 			c.JSON(http.StatusOK, gin.H{"task": task.Pattern})
@@ -68,8 +102,8 @@ func updateTask(c *gin.Context) {
 	defer tasksLock.Unlock()
 
 	var request struct {
-		Task   string `json:"task"`
-		Status int    `json:"status"`
+		Task       string `json:"task"`
+		WorkerName string `json:"worker_name"`
 	}
 
 	if err := c.BindJSON(&request); err != nil {
@@ -78,8 +112,9 @@ func updateTask(c *gin.Context) {
 	}
 
 	for i, task := range tasks {
-		if task.Pattern == request.Task && task.WorkerName == c.ClientIP() && task.Status == 1 {
-			task.Status = request.Status
+		if task.Pattern == request.Task && task.WorkerName == request.WorkerName && task.Status == PROCESSING {
+			task.Status = FINISHED
+			task.FinishedTime = time.Now().In(time.FixedZone("CST", 8*3600)).Format("2006-01-02 15:04:05")
 			tasks[i] = task
 			saveTasks()
 			c.JSON(http.StatusOK, gin.H{"message": "Task updated"})
@@ -93,10 +128,13 @@ func withdrawTask(c *gin.Context) {
 	tasksLock.Lock()
 	defer tasksLock.Unlock()
 
-	for _, task := range tasks {
-		if task.Status == 1 && task.WorkerName == c.ClientIP() {
-			task.Status = 0
-
+	for i, task := range tasks {
+		if task.Status == PROCESSING && task.WorkerName == c.ClientIP() {
+			task.Status = UNFINISHED
+			task.FinishedTime = ""
+			task.AssignedTime = ""
+			task.WorkerName = ""
+			tasks[i] = task
 		}
 	}
 
@@ -108,11 +146,22 @@ func withdrawTask(c *gin.Context) {
 func saveTasks() {
 	data, err := json.Marshal(tasks)
 	if err != nil {
-		fmt.Println("Error marshalling tasks:", err)
+		logrus.WithError(err).Error("Error marshalling tasks")
 		return
 	}
 	err = os.WriteFile("task.json", data, 0644)
 	if err != nil {
-		fmt.Println("Error writing tasks file:", err)
+		logrus.WithError(err).Error("Error writing tasks file")
+	}
+}
+
+func logRequest(handlerFunc gin.HandlerFunc) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		logrus.WithFields(logrus.Fields{
+			"method": c.Request.Method,
+			"path":   c.Request.URL.Path,
+			"ip":     c.ClientIP(),
+		}).Info("Received request")
+		handlerFunc(c)
 	}
 }
